e9f81e2b6a8b30d1c8b131e460e5aa17f7f37311
Varun Sahni <varunsahni3504@gmail.com>
2025-02-02 06:19:17 +0000

    feat: diff modality

--- main_mist.py
+++ main_mist.py
@@ -1,7 +1,9 @@
 from dataset_ucsf import BrainDataset, clean_data
+from dataset import BrainRSNADataset
 from modelv2 import ViT3D
 from torch.utils.data import DataLoader, random_split, WeightedRandomSampler
 import torch
+from utils import compute_metrics
 import lightning as L
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers.tensorboard import TensorBoardLogger
@@ -9,8 +11,10 @@ from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from torch.utils.data.distributed import DistributedSampler 
 import config
 import pandas as pd
+from monai.networks.nets import DenseNet121
 from sklearn.model_selection import train_test_split
 import torch.nn as nn
+from modify_model import get_model_upto_layer
 from collections import namedtuple
 
 
@@ -18,6 +22,7 @@ from collections import namedtuple
 # DIM / IMAGE SIZE ONLY 128 MAYBE UPSCALE IF NEEDED
 # Load configuration
 config = config.get_mgmt_config()
+
 
 file_path = '/scratch/p/ptyrrell/vsahni3'
 # Callbacks
@@ -33,23 +38,22 @@ checkpoint_callback = ModelCheckpoint(
 
 
 
-def reset_weights(m):
-    """
-    Resets the weights of a model using appropriate initialization methods.
-    """
-    if isinstance(m, (nn.Conv3d, nn.Conv2d, nn.Conv1d, nn.Linear)):
-        nn.init.xavier_uniform_(m.weight)  # Xavier initialization for weights
-        if m.bias is not None:
-            nn.init.zeros_(m.bias)  # Reset bias to zero
-    elif isinstance(m, (nn.BatchNorm3d, nn.BatchNorm2d, nn.BatchNorm1d, nn.LayerNorm)):
-        nn.init.ones_(m.weight)  # Set scaling factor to 1
-        nn.init.zeros_(m.bias)  # Reset bias to zero
+def create_sampler(train_df):
+    num_negative = len(train_df[train_df[config.target] == 0])
+    num_positive = len(train_df) - num_negative
+
+    # neg must be first cuz of indexing by label
+    class_counts = [num_negative, num_positive]
+    class_weights = 1. / torch.tensor(class_counts, dtype=torch.float)
+    sample_weights = [class_weights[int(label)] for label in train_df[config.target]]
+    sampler = WeightedRandomSampler(weights=sample_weights, num_samples=len(sample_weights), replacement=True)
+    return sampler
+
 
 # Instantiate the model
 
 # model.apply(reset_weights)
 Params = namedtuple("Params", ["lr", "drop", "sched_type", "patience", "factor", "weight_decay", "img_types", "label_smoothing"])
-# change validation, make it balanced
 # label smoothing
 # stochatsic depth
 # precision affects
@@ -57,24 +61,23 @@ Params = namedtuple("Params", ["lr", "drop", "sched_type", "patience", "factor",
 #AJWIDNWEFNIEFNEOJFKEFMEMFE
 
 params_list = [
-    Params(lr=1e-4, drop=0.1, sched_type='train_loss', patience=25, factor=0.05, weight_decay=1e-3, img_types=("T1c", "SWI"), label_smoothing=0.0),
-    Params(lr=1e-4, drop=0.0, sched_type='train_loss', patience=25, factor=0.2, weight_decay=1e-3, img_types=("T1c", "SWI"), label_smoothing=0.0),
-    Params(lr=1e-4, drop=0.0, sched_type='train_loss', patience=25, factor=0.05, weight_decay=1e-3, img_types=("T1c", "SWI"), label_smoothing=0.1)
+    Params(lr=1e-4, drop=0.1, sched_type='train_loss', patience=25, factor=0.05, weight_decay=1e-3, img_types=("T1c", "T1"), label_smoothing=0.0),
+    Params(lr=1e-4, drop=0.0, sched_type='train_loss', patience=25, factor=0.05, weight_decay=1e-3, img_types=("T1c", "T1"), label_smoothing=0.1),
+    Params(lr=1e-4, drop=0.0, sched_type='train_loss', patience=25, factor=0.05, weight_decay=1e-3, img_types=("T1c", "T1"), label_smoothing=0.0)
 ]
 
 legend = 'lr drop val_or_train_sched patience factor weight_decay shape img'
 for params in params_list:
     params_title = f'{params.lr} {params.drop} {params.sched_type} {params.patience} {params.factor} {params.weight_decay} {params.img_types} {params.label_smoothing}'
     logger = TensorBoardLogger(save_dir=f"{file_path}/lightning_logs", name=f"vit_model_{params_title}")
-    # Model
-    # model = ViT(config)
+
     model = ViT3D(
         config=config,
         num_classes=2, 
         add_cls_token=True,
         dropout=params.drop,
-        lr=params.lr,
         label_smoothing=params.label_smoothing,
+        lr=params.lr,
         weight_decay=params.weight_decay,
         optimizer_params={
             'type': params.sched_type,
@@ -92,18 +95,12 @@ for params in params_list:
 
 
     train_df, tmp_df = train_test_split(data, test_size=0.3, random_state=6969)
+    sampler = create_sampler(train_df)
 
-
-    num_negative = len(train_df[train_df[config.target] == 0])
-    num_positive = len(train_df) - num_negative
-
-    # neg must be first cuz of indexing by label
-    class_counts = [num_negative, num_positive]
-    class_weights = 1. / torch.tensor(class_counts, dtype=torch.float)
-    sample_weights = [class_weights[int(label)] for label in train_df[config.target]]
-    sampler = WeightedRandomSampler(weights=sample_weights, num_samples=len(sample_weights), replacement=True)
+    
 
     val_df, test_df = train_test_split(tmp_df, test_size=0.5, random_state=6969)
+
 
     train_dataset = BrainDataset(data=train_df, is_train=True, types=params.img_types)
     val_dataset = BrainDataset(data=val_df, is_train=False, types=params.img_types)
@@ -123,7 +120,11 @@ for params in params_list:
     logger=logger,
     accelerator="auto",
     devices=4,
-    callbacks=[checkpoint_callback],
-    num_nodes=1
+    callbacks=[checkpoint_callback]
     )
     trainer.fit(model, train_loader, val_loader)
+
+
+
+
+
